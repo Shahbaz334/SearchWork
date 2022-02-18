7ae37fdc1d22ddc5e760cfbc14803c198cf5b6a4
saeedahmed2023 <87636516+saeedahmed2023@users.noreply.github.com>
2022-02-18 21:28:46 +0500

    ios icon change and language picker set

--- src/Components/organisms/LanguagePicker.js
+++ src/Components/organisms/LanguagePicker.js
@@ -1,92 +1,100 @@
-import React from 'react';
-import { StyleSheet, Image, TouchableOpacity, Text } from 'react-native';
-import LocalStrings from '../../Language/Strings'
-import ModalDropdown from 'react-native-modal-dropdown';
+import React from "react";
+import { StyleSheet, Image, TouchableOpacity, Text } from "react-native";
+import LocalStrings from "../../Language/Strings";
+import ModalDropdown from "react-native-modal-dropdown";
 
 // Redux
-import { useDispatch, useSelector } from 'react-redux';
-import { setLanguage, setLanguageSelected } from '../../redux/slices';
+import { useDispatch, useSelector } from "react-redux";
+import { setLanguage, setLanguageSelected } from "../../redux/slices";
 
 const items = [
   {
-    label: 'English',
-    value: 'en',
-    icon: require('../../../assets/usa.png')
+    label: "English",
+    value: "en",
+    icon: require("../../../assets/usa.png"),
   },
   {
-    label: 'Spanish',
-    value: 'sp',
-    icon: require('../../../assets/spain.png')
+    label: "Spanish",
+    value: "sp",
+    icon: require("../../../assets/spain.png"),
   },
-]
+];
 
 const LanguagePicker = () => {
   // Redux
-  let selectedLang = useSelector(setLanguageSelected)
+  let selectedLang = useSelector(setLanguageSelected);
   // console.log('============',selectedLang)
   const dispatch = useDispatch();
   // Local State
-  const [isOpen, setIsOpen] = React.useState(false)
-  const [lang, setLang] = React.useState(selectedLang === 'en' ? 'English' : 'Spanish')
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [lang, setLang] = React.useState(
+    selectedLang === "en" ? "English" : "Spanish"
+  );
 
   return (
     <ModalDropdown
       options={items}
-      defaultValue = {lang}
-      style = {[styles.btnContainer, {
-        borderBottomLeftRadius: isOpen ? 0 : 25,
-        borderBottomRightRadius: isOpen ? 0 : 25,
-      }]}
-      textStyle = {[styles.textStyle, {
-        borderBottomLeftRadius: isOpen ? 0 : 25,
-        borderBottomRightRadius: isOpen ? 0 : 25,
-      }]}
-      dropdownStyle = {styles.dropdownStyle}
-
+      defaultValue={lang}
+      style={[
+        styles.btnContainer,
+        {
+          borderBottomLeftRadius: isOpen ? 0 : 25,
+          borderBottomRightRadius: isOpen ? 0 : 25,
+        },
+      ]}
+      textStyle={[
+        styles.textStyle,
+        {
+          borderBottomLeftRadius: isOpen ? 0 : 25,
+          borderBottomRightRadius: isOpen ? 0 : 25,
+        },
+      ]}
+      dropdownStyle={styles.dropdownStyle}
       renderRow={(data) => {
         return (
-          <TouchableOpacity style = {styles.row} onPress={() => {
-            setLang(data.label)
-            LocalStrings.setLanguage(data.value)
-            dispatch(setLanguage(data.value))
-            
-          }}>
-            <Image source={data.icon} style = {styles.iconImage} />
-            <Text style = {{paddingHorizontal: 5}}>{data.label}</Text>
+          <TouchableOpacity
+            style={styles.row}
+            onPress={() => {
+              setLang(data.label);
+              LocalStrings.setLanguage(data.value);
+              dispatch(setLanguage(data.value));
+            }}
+          >
+            <Image source={data.icon} style={styles.iconImage} />
+            <Text style={{ paddingHorizontal: 5 }}>{data.label}</Text>
           </TouchableOpacity>
-        )
+        );
       }}
-
-      onDropdownWillShow = {() => {
-        setIsOpen(true)
+      onDropdownWillShow={() => {
+        setIsOpen(true);
       }}
-
-      onDropdownWillHide = {() => {
-        setIsOpen(false)
+      onDropdownWillHide={() => {
+        setIsOpen(false);
       }}
     />
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   btnContainer: {
     width: 100,
     height: 50,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderWidth: 1,
-    borderColor: '#000',
+    borderColor: "#000",
     borderTopRightRadius: 30,
     borderTopLeftRadius: 30,
   },
 
   textStyle: {
+    marginTop: 17,
     width: 98,
     fontSize: 14,
-    height: 49,
+    height: 33,
     borderTopRightRadius: 30,
     borderTopLeftRadius: 30,
     textAlign: "center",
-    textAlignVertical: 'center',
+    textAlignVertical: "center",
   },
 
   dropdownStyle: {
@@ -94,7 +102,7 @@ const styles = StyleSheet.create({
     width: 100,
     padding: 5,
     borderWidth: 1,
-    borderColor: '#000',
+    borderColor: "#000",
     borderBottomLeftRadius: 10,
     borderBottomRightRadius: 10,
     marginTop: 2,
@@ -103,15 +111,15 @@ const styles = StyleSheet.create({
   iconImage: {
     height: 16,
     width: 20,
-    paddingHorizontal: 15
+    paddingHorizontal: 15,
   },
 
   row: {
     width: 85,
-    flexDirection: 'row',
+    flexDirection: "row",
     paddingVertical: 5,
-    marginVertical: 2
-  }
+    marginVertical: 2,
+  },
 });
 
 export default LanguagePicker;
